dfbc09cea2824204808f39701f322f996ab7f7e6
oluwaseunisrael <111712379+oluwaseunisrael@users.noreply.github.com>
2026-08-30 00:10:23 +0100

    Update pool.js

--- pool.js
+++ pool.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
-
 const { Pool } = require('pg');
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set');
+  console.error('WARNING: DATABASE_URL is not set.');
 }
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected Postgres pool error:', err.message);
 });
 
 module.exports = pool;
