640b3cedbd37119944dc89dce4bbf3355aef0022
oluwaseunisrael <111712379+oluwaseunisrael@users.noreply.github.com>
2026-08-29 22:59:43 +0100

    Change server port from 3000 to 1000

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Render provides PORT through environment variables.
 // Locally, it will use port 3000.
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1000;
 
 // Middleware
 app.use(express.json());
